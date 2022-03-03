ed8dc41915bef2dd680f04379ed84d80e85a4984
Khemchand Ojha <ojha.akash876@gmail.com>
2022-03-03 16:05:05 +0530

    final websocket

--- server/index.js
+++ server/index.js
@@ -1,14 +1,22 @@
-const { lchown } = require("fs");
+// const { lchown } = require("fs");
 const http = require("http");
 const ws = require("ws");
 
 const server = http.createServer((req, res) => {
+  
   res.end("Server has connected");
-});
+}).listen(3008);
 
 const wss = new ws.Server({ server });
-wss.on("headers", (head) => {
-  console.log(head);
+wss.on("headers", (msg,req) => {
+  // console.log(msg);
+  // console.log(req);
 });
 
-server.listen(3008);
+ wss.on("connection", (webs,req) => {
+   webs.send("Hello i am ready to work!");
+  //  console.log(webs)
+   webs.on("message", (msg) => { console.log(msg)})
+})
+
+
